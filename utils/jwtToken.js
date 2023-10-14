98cccf661d87fcf93c7b45493972d89962abb855
akshat2002aj <81851184+akshat2002aj@users.noreply.github.com>
2023-10-15 01:29:15 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,7 +6,9 @@ const sendToken = (user, statusCode, res)=>{
     // options for cookies
     const options = {
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-        httpOnly: true
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
     }
 
     res.status(statusCode).cookie("token", token, options).json({
